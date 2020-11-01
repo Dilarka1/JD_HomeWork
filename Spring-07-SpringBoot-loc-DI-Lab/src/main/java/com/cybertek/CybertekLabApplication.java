dff74e76f5532816af0ea085d08317c4d4777dcb
Dilara Khafizov <dilara_khafizova@gmail.com>
2020-10-31 22:50:18 -0400

    carpet projet Ozzy

--- Spring-07-SpringBoot-loc-DI-Lab/src/main/java/com/cybertek/CybertekLabApplication.java
+++ Spring-07-SpringBoot-loc-DI-Lab/src/main/java/com/cybertek/CybertekLabApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarpetSolutionApplication {
+public class CybertekLabApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarpetSolutionApplication.class, args);
+		SpringApplication.run(CybertekLabApplication.class, args);
 	}
 
 }
